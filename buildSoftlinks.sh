dc9006348a48eddb50834f5b3c13d4bda4f081a7
hk416hasu <vbcgfh487882183@126.com>
2024-08-28 21:25:11 +0800

    update a few

--- buildSoftlinks.sh
+++ buildSoftlinks.sh
@@ -1,5 +1,9 @@
 #!/bin/env bash
 
+# !! SHOULD BE UPDATED !!
+# !! SHOULD BE UPDATED !!
+# !! SHOULD BE UPDATED !!
+
 # plz run this script in 'dotfiles' directory
 
 fileArray=("bashrc" "inputrc" "vimrc" "tmux.conf" "gitconfig")
@@ -18,6 +22,10 @@ ln -s "$PWD/dosbox-0.74-3.conf" "$HOME/.dosbox/dosbox-0.74-3.conf"
 
 if [[ -f "/etc/tlp.conf" ]]; then rm /etc/tlp.conf; fi
 ln -s "$PWD/tlp.conf" "/etc/tlp.conf"
+
+#keyboardPath="/etc/default/keyboard"
+#if [[ -f "$keyboardPath" ]]; then rm "$keyboardPath"; fi
+#ln -s "$PWD/keyboard" "$keyboardPath"
 
 # /root:
 sudo rm "/root/.vimrc"
